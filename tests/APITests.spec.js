c6cb6dfad365fc076339365e26c05049b72edc2c
Vikas V <138219762+Vikasv360@users.noreply.github.com>
2025-01-31 16:49:59 +0530

    Update APITests.spec.js

--- tests/APITests.spec.js
+++ tests/APITests.spec.js
@@ -3,8 +3,8 @@ const { test, expect } = require('@playwright/test');
 var userToken;
 var bookId;
 var orderId;
-const customerName = "vikasv385";
-const customerEmail = "vikasv385@example.com";
+const customerName = "vikasv386";
+const customerEmail = "vikasv386@example.com";
 
 test.beforeAll('POST : Customer Authorization', async ({ request }) => {
 
